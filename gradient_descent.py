8a3c0cffe92cbb3928b043570a40e718f370333a
Luca Marini <luca.marini9@studio.unibo.it>
2021-04-28 16:27:25 +0200

    from 2D to 3D gradient descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,45 +1,50 @@
 import numpy as np
 import utils
+from random import randrange
 
 
-def optimize(func_grad, lr=0.01, eps=1e-8, max_iter=10000, tol=1e-6):
+def optimize(func_grad, lr=0.01, max_iter=10000, tol=1e-6):
     print('gradient_descent')
     # constants
+    theta_1 = randrange(10)
+    theta_2 = randrange(10)
 
-    theta = 0
-    s_t = 0
+    # lists that contain theta_1, theta_2 values and the iterations
+    theta_values_1, theta_values_2, count = np.empty(0), np.empty(0), np.empty(0)
     # num of iteration
     t = 1
     # learning rate
     lr = lr
-    eps = eps
     # max iteration stopping criterion
     max_iter = max_iter
-    # velocity at iteration t
-    v_t = 0
-    # list that contains all theta values
-    theta_values = []
-    # list that contains all velocity values
-    velocity_values = []
+
 
     # list containing all loss values
     # loss_values = []
-    theta_prev = -10
+    theta_prev_1 = -10
+    theta_prev_2 = -10
 
-    while np.abs(theta - theta_prev) > tol and t <= max_iter:
-        theta_values.append(theta)
+    while (np.abs(theta_1 - theta_prev_1) > tol
+           and np.abs(theta_2 - theta_prev_2) > tol) \
+            and t <= max_iter:
+        theta_values_1 = np.append(theta_values_1, theta_1)
+        theta_values_2 = np.append(theta_values_2, theta_2)
+        count = np.append(count, t)
         # loss_values.append(np.power(theta - main.y, 2))
 
-        grad = func_grad(theta)
+        grad_1, grad_2 = func_grad(theta_1, theta_2)
 
-        theta_prev = theta
-        theta = theta - lr * grad
+        theta_prev_1 = theta_1
+        theta_prev_2 = theta_2
+        theta_1 = theta_1 - lr * grad_1
+        theta_2 = theta_2 - lr * grad_2
         t += 1
 
-    if np.abs(theta - theta_prev) <= tol:
+    if (np.abs(theta_1 - theta_prev_1) <= tol
+           or np.abs(theta_2 - theta_prev_2) <= tol):
         print(' - tol')
-        utils.print_result(t, theta)
+        utils.print_result(t, (theta_1, theta_2))
     if t > max_iter:
         print(' - max iterations reached')
-        utils.print_result(t, theta)
-    return theta_values  # , loss_values
+        utils.print_result(t, (theta_1, theta_2))
+    return theta_values_1, theta_values_2, count  # , loss_values
