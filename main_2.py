e9c9c62996380c393c8634a2bf372a8615948a6b
Luca Marini <luca.marini9@studio.unibo.it>
2021-04-29 09:28:13 +0200

    add other optimizers

--- main_2.py
+++ main_2.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+import adagrad_2
+import adam_2
 import gradient_descent
 import adam
 import momentum
@@ -13,10 +15,12 @@ import numpy as np
 import functions as func
 from random import randrange
 
+import rms_prop_2
+
 if __name__ == '__main__':
     # constants
     lr = 0.1
-    max_iter = 100
+    max_iter = 1000
     tol = 1e-6
 
     # initial values of theta_1 and theta_2
@@ -41,12 +45,36 @@ if __name__ == '__main__':
                                          tol=tol)
     # Nesterov momentum
     nesterov_mom_theta_values_1, nesterov_mom_theta_values_2, \
-    nesterov_mom_count = nesterov_momentum_2.optimize(func.grad_paraboloid,
+        nesterov_mom_count = nesterov_momentum_2.optimize(func.grad_paraboloid,
+                                                          init_theta_1=theta_1,
+                                                          init_theta_2=theta_2,
+                                                          beta=0.9,
+                                                          lr=lr, max_iter=max_iter,
+                                                          tol=tol)
+    # adagrad
+    adagrad_theta_values_1, adagrad_theta_values_2, \
+        adagrad_count = adagrad_2.optimize(func.grad_paraboloid,
+                                           init_theta_1=theta_1,
+                                           init_theta_2=theta_2,
+                                           lr=lr, eps=1e-8, max_iter=max_iter,
+                                           tol=tol)
+    # rmsprop
+    rms_prop_theta_values_1, rms_prop_theta_values_2, \
+        rms_prop_count = rms_prop_2.optimize(func.grad_paraboloid,
                                              init_theta_1=theta_1,
                                              init_theta_2=theta_2,
                                              beta=0.9,
-                                             lr=lr, max_iter=max_iter,
+                                             lr=lr, eps=1e-8, max_iter=max_iter,
                                              tol=tol)
+    # adam
+    adam_theta_values_1, adam_theta_values_2, \
+        adam_count = adam_2.optimize(func.grad_paraboloid,
+                                     init_theta_1=theta_1,
+                                     init_theta_2=theta_2,
+                                     beta_1=0.9, beta_2=0.999,
+                                     lr=lr, eps=1e-8, max_iter=max_iter,
+                                     tol=tol)
+
     # defining the 3D space where to compute the function
     x = np.linspace(-2, 4, 250)
     y = np.linspace(-2, 4, 250)
@@ -66,18 +94,31 @@ if __name__ == '__main__':
     ax.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap='jet', alpha=.4, edgecolor='none')
 
     # ________________________________________plotting optimizers________________________________________
-    # gradient descent
+    # gradient descent 3D plot
     ax.plot(gd_theta_values_1, gd_theta_values_2,
             func.paraboloid(gd_theta_values_1, gd_theta_values_2),
-            color='r', marker='*', alpha=.4)
-    # momentum
+            color='r', marker='.', alpha=.4, label='gradient_descent')
+    # momentum 3D plot
     ax.plot(momentum_theta_values_1, momentum_theta_values_2,
             func.paraboloid(momentum_theta_values_1, momentum_theta_values_2),
-            color='b', marker='*', alpha=.4)
+            color='purple', marker='.', alpha=.4, label='momentum')
+    # Nesterov momentum 3D plot
     ax.plot(nesterov_mom_theta_values_1, nesterov_mom_theta_values_2,
             func.paraboloid(nesterov_mom_theta_values_1, nesterov_mom_theta_values_2),
-            color='black', marker='*', alpha=.4)
-
+            color='black', marker='.', alpha=.4, label='Nesterov_momentum')
+    # adagrad 3D plot
+    ax.plot(adagrad_theta_values_1, adagrad_theta_values_2,
+            func.paraboloid(adagrad_theta_values_1, adagrad_theta_values_2),
+            color='violet', marker='.', alpha=.4, label='adagrad')
+    # rms_prop 3D plot
+    ax.plot(rms_prop_theta_values_1, rms_prop_theta_values_2,
+            func.paraboloid(rms_prop_theta_values_1, rms_prop_theta_values_2),
+            color='b', marker='.', alpha=.4, label='rms_prop')
+    # adam 3D plot
+    ax.plot(adam_theta_values_1, adam_theta_values_2,
+            func.paraboloid(adam_theta_values_1, adam_theta_values_2),
+            color='orange', marker='.', alpha=.4, label='adam')
+    ax.legend()
     ax.view_init(45, 280)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
@@ -90,7 +131,7 @@ if __name__ == '__main__':
     ax.scatter(gd_theta_values_1, gd_theta_values_2, color='r', marker='*')
     # momentum
     ax.scatter(momentum_theta_values_1, momentum_theta_values_2, color='b', marker='*')
-    plt.legend()
+
 
     ax.quiver(gd_theta_values_1[:-1], gd_theta_values_2[:-1], anglesx, anglesy,
               scale_units='xy', angles='xy', scale=1,
